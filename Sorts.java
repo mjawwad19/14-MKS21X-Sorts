2db38faef30e896173f7631e38ecd5d5e02c8878
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-07 10:57:21 -0500

    Updated prompts based on website update, and comments + added fast bubbleSort test case

--- Sorts.java
+++ Sorts.java
@@ -1,13 +1,9 @@
 import java.util.*;
 import java.io.*;
-/*
-*@param ary is any array to be sorted.
-Directions:
-choose the smallest put it into ary[0]
-choose the next smallest put it into ary[1]  (you can ignore the slots before the current position)
-choose the next smallest put it into ary[2]  (you can ignore the slots before the current position)
-...
-*/
+/**Selection sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
 public class Sorts {
   public static void selectionSort(int [] ary) {
     int least,temp;
@@ -25,7 +21,10 @@ public class Sorts {
     //System.out.println(Arrays.toString(ary));
   }
 }
-
+/**Bubble sort of an int array.
+*Upon completion, the elements of the array will be in increasing order.
+*@param data  the elements to be sorted.
+*/
   public static void bubbleSort(int [] ary) {
     int least, temp;
     for (int i = 0; i < ary.length; i++) {
@@ -33,12 +32,15 @@ public class Sorts {
       least = i;
       for (int j = 0; j < end; j++) {
         if (ary[j] > ary[least]) {
+          //swaqpping two vals
           temp = ary[j];
           ary[j] = ary[least];
           ary[least] = temp;
+          //considering if I should just make a swap method
         }
       }
       end--;
+      //we know the last value will be the largest so we don't need to iterate to it again
     }
   }
 
@@ -47,6 +49,7 @@ public class Sorts {
     int[] ary2 = {5,1,12,-5,16,2,12,14};
     int[] ary3 = {5,1,12,-5,16};
     int[] ary4 = {2,3,4,5,1};
+    int[] ary5 = {6,1,2,3,4,5};
     System.out.println("This is your original: " + Arrays.toString(ary1));
     //I'm just too lazy to copy over toString AGAIN
     selectionSort(ary1);
@@ -60,6 +63,9 @@ public class Sorts {
     System.out.println("This is your original: " + Arrays.toString(ary4));
     bubbleSort(ary4);
     System.out.println("Bubble Sorted: " + Arrays.toString(ary4));
+    System.out.println("This is your original: " + Arrays.toString(ary5));
+    bubbleSort(ary5);
+    System.out.println("Bubble Sorted: " + Arrays.toString(ary5));
 
   }
 }
