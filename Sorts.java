973a2c25e029207198fa1e996b3689c03189906b
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-11 20:25:22 -0500

    Fixed selectionSort on sorted lists.

--- Sorts.java
+++ Sorts.java
@@ -7,9 +7,16 @@ import java.io.*;
   */
 
 public class Sorts {
+  //helper to check if sorted or not and not waste time
+private static boolean isSorted(int[] data) {
+  for (int i = 0; i < data.length -1; i++) {
+    if (data[i] > data[i+1]) return false;
+  }
+  return true;
+}
   public static void selectionSort(int [] data) {
     int least,temp;
-    //advised by others that I should not use Math.min :/
+    if (isSorted(data)) return;
     for (int i = 0; i < data.length; i++) {
       least = i;
       for (int j = i; j < data.length; j++) {
@@ -29,7 +36,7 @@ public class Sorts {
 */
   public static void bubbleSort(int [] data) {
     int least, temp;
-    boolean madeSwaps;;
+    boolean madeSwaps;
     for (int i = 0; i < data.length; i++) {
       int end = data.length -1;
       least = i;
@@ -86,27 +93,28 @@ public class Sorts {
     int[] ary5 = {6,1,2,3,4,5};
     int[] ary6 = {4,3,2,10,12,1,5,6};
     int[] mill = new int[1000000];
+    int[] ary7 = {2,3,4,5,6};
     System.out.println("This is your original: " + Arrays.toString(ary1));
     //I'm just too lazy to copy over toString AGAIN
     selectionSort(ary1);
-    System.out.println("Selection Sorted: " + Arrays.toString(ary1));
+    System.out.println("Selection Sorted: " + Arrays.toString(ary1) +'\n');
     System.out.println("This is your original: " + Arrays.toString(ary2));
     selectionSort(ary2);
-    System.out.println("Selection Sorted: " + Arrays.toString(ary2));
+    System.out.println("Selection Sorted: " + Arrays.toString(ary2)+'\n');
     System.out.println("This is your original: " + Arrays.toString(ary3));
     bubbleSort(ary3);
-    System.out.println("Bubble Sorted: " + Arrays.toString(ary3));
+    System.out.println("Bubble Sorted: " + Arrays.toString(ary3)+'\n');
     System.out.println("This is your original: " + Arrays.toString(ary4));
     bubbleSort(ary4);
-    System.out.println("Bubble Sorted: " + Arrays.toString(ary4));
+    System.out.println("Bubble Sorted: " + Arrays.toString(ary4)+'\n');
     System.out.println("This is your original: " + Arrays.toString(ary5));
     bubbleSort(ary5);
-    System.out.println("Bubble Sorted: " + Arrays.toString(ary5));
+    System.out.println("Bubble Sorted: " + Arrays.toString(ary5)+'\n');
     System.out.println("This is your orignal: " + Arrays.toString(ary6));
     insertionSort(ary6);
-    System.out.println("Insertion Sorted: " + Arrays.toString(ary6));
+    System.out.println("Insertion Sorted: " + Arrays.toString(ary6)+'\n');
     bubbleSort(mill);
     selectionSort(ary7);
-    System.out.println("Selection Sorted: " + Arrays.toString(ary7));
+    System.out.println("Selection Sorted: " + Arrays.toString(ary7)+'\n');
   }
 }
